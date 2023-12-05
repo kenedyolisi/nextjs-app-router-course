73aa089acebe6730b223f3c8321013a008b0d15c
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2023-12-05 15:09:32 +0300

    feat: fetch data for revenue chart

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,10 @@
+import { fetchRevenue } from '../lib/data';
+import RevenueChart from '../ui/dashboard/revenue-chart';
 import { lusitana } from '../ui/fonts';
 
 export default async function DashboardPage() {
+  const revenue = await fetchRevenue();
+
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -17,7 +21,7 @@ export default async function DashboardPage() {
         /> */}
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        {/* <RevenueChart revenue={revenue}  /> */}
+        <RevenueChart revenue={revenue}  />
         {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
       </div>
     </main>
